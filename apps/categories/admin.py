e82f7122a1941a15fe56a0d88966682e5d84301a
ablokulov <abloqulovnodirbek4@gmail.com>
2026-03-10 10:35:34 +0500

    feat(categories): get categories model and slug

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from .models import Category
 
 
-admin.site.register(Category)
+# admin.site.register(Category)
 # Register your models here.
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    
+    list_display = (
+        "id",
+        "name",
+        "slug",
+        "is_active"
+    )
