15bb9f212fad46e5dd637a65e2eb7cb1debc1046
ablokulov <abloqulovnodirbek4@gmail.com>
2026-03-10 10:37:22 +0500

    feat(reviews): reviews  endpoint validation

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Review
 from .serializers import ReviewSerializer, ReviewCreateSerializer
@@ -7,7 +7,7 @@ from .serializers import ReviewSerializer, ReviewCreateSerializer
 
 class ReviewListCreateView(ListCreateAPIView):
 
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
 
